47c10cfdd229972ac5cc453ad5e99fa58b66b537
Manas Nanivadekar <69134799+Manas-Nanivadekar@users.noreply.github.com>
2023-01-31 02:16:16 +0530

    client add

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import boto3
 
-s3 = boto3.resource('s3')
+s3 = boto3.client('s3')
 s3.download_file(sys.argv[1], sys.argv[2], sys.argv[3])
 # sys.argv[1] = bucket name
 # sys.argv[2] = file name
