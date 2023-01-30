505cb083bf54120b5a102abb338df77dda77353f
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2023-01-30 18:52:44 +0530

    final code add

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import boto3
 
 s3 = boto3.resource('s3')
 s3.download_file(sys.argv[1], sys.argv[2], sys.argv[3])
+# sys.argv[1] = bucket name
+# sys.argv[2] = file name
+# sys.argv[3] = local file name
